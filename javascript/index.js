2b8c5bf0f8dc7c2d6be4657b3f8054b38a9c9976
gargamal <pellierdaniel@free.fr>
2017-10-29 22:42:34 +0100

    [UPDATE PAGE] - Mise à jour du menu avec l'action contact - Mise à jour du code js (action onresize) - Mise à jour des diaporamma

--- javascript/index.js
+++ javascript/index.js
@@ -1,42 +1,66 @@
-function displayNavBar(index) {
-    var contactMe = document.getElementById("contactMe");
-    var myTwitterLink = document.getElementById("myTwitterLink");
-    var myFacebookLink = document.getElementById("myFacebookLink");
-    var phoneNumber = document.getElementById("phoneNumber");
+function Display() {
+    var createPhoneNumber = function (myLinkNavBar) {
+        var phoneNumber = document.createElement("li");
+        phoneNumber.id = "myPhoneNumber";
+        myLinkNavBar.appendChild(phoneNumber);
+        return phoneNumber;
+    };
 
-    if (window.innerWidth < 992 && (contactMe || myTwitterLink || myFacebookLink)) {
-        contactMe.parentNode.removeChild(contactMe);
-        myTwitterLink.parentNode.removeChild(myTwitterLink);
-        myFacebookLink.parentNode.removeChild(myFacebookLink);
-        phoneNumber.setAttribute("href", "tel:+33243237149");
-
-    } else if (window.innerWidth >= 992 && (!myTwitterLink || !myFacebookLink)) {
+    var fctNavBar = function (index) {
         var myLinkNavBar = document.getElementById("myLinkNavBar");
+        var myContactMe = document.getElementById("myContactMe");
+        var myTwitterLink = document.getElementById("myTwitterLink");
+        var myFacebookLink = document.getElementById("myFacebookLink");
+        var phoneNumber = document.getElementById("myPhoneNumber");
+        if (!phoneNumber) {
+            phoneNumber = createPhoneNumber(myLinkNavBar);
+        }
 
-        var liContactMe = document.createElement("li");
-        liContactMe.id = "contactMe";
-        liContactMe.innerHTML = "<a href='#'><span class='fa fa-envelope' aria-hidden='true'></span></a>";
-        myLinkNavBar.appendChild(liContactMe);
+        if (window.innerWidth < 993) {
+            if (myContactMe || myTwitterLink || myFacebookLink) {
+                myContactMe.parentNode.removeChild(myContactMe);
+                myTwitterLink.parentNode.removeChild(myTwitterLink);
+                myFacebookLink.parentNode.removeChild(myFacebookLink);
+            }
 
-        var liTwitterLink = document.createElement("li");
-        liTwitterLink.id = "myTwitterLink";
-        liTwitterLink.innerHTML = "<a href='#'><span class='fa fa-twitter' aria-hidden='true'></span></a>";
-        myLinkNavBar.appendChild(liTwitterLink);
+            phoneNumber.innerHTML = "<a href='tel:+33243237149'><span class='fa fa-phone' aria-hidden='true'></span><span> 02 43 23 71 49</span></a>";
 
-        var liFacebookLink = document.createElement("li");
-        liFacebookLink.id = "myFacebookLink";
-        liFacebookLink.innerHTML = "<a href='#'><span class='fa fa-facebook-official' aria-hidden='true'></span></a>";
-        myLinkNavBar.appendChild(liFacebookLink);
+        } else if (window.innerWidth >= 993) {
+            if (!myContactMe) {
+                var limyContactMe = document.createElement("li");
+                limyContactMe.id = "myContactMe";
+                limyContactMe.innerHTML = "<a href='#'><span class='fa fa-envelope' aria-hidden='true'></span><span> contactez nous</span></a>";
+                myLinkNavBar.appendChild(limyContactMe);
+            }
 
-        phoneNumber.setAttribute("href", "#");
+            if (!myTwitterLink) {
+                var liTwitterLink = document.createElement("li");
+                liTwitterLink.id = "myTwitterLink";
+                liTwitterLink.innerHTML = "<a href='#'><span class='fa fa-twitter' aria-hidden='true'></span></a>";
+                myLinkNavBar.appendChild(liTwitterLink);
+            }
 
-        var nivBar = document.getElementById("menuNavBar");
-        var liMenu = nivBar.getElementsByTagName("li");
-        for (var i = 0; i < liMenu.length; i++) {
-            if (i === index) {
-                liMenu[i].setAttribute("class", "active");
-                break;
+            if (!myFacebookLink) {
+                var liFacebookLink = document.createElement("li");
+                liFacebookLink.id = "myFacebookLink";
+                liFacebookLink.innerHTML = "<a href='#'><span class='fa fa-facebook-official' aria-hidden='true'></span></a>";
+                myLinkNavBar.appendChild(liFacebookLink);
+            }
+
+            phoneNumber.innerHTML = "<a href='#'><span class='fa fa-phone' aria-hidden='true'></span></a>";
+
+            var nivBar = document.getElementById("menuNavBar");
+            var liMenu = nivBar.getElementsByTagName("li");
+            for (var i = 0; i < liMenu.length; i++) {
+                if (i === index) {
+                    liMenu[i].setAttribute("class", "active");
+                    break;
+                }
             }
         }
+    };
+
+    return {
+        navBar: fctNavBar
     }
 }
