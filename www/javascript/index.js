ff4175209c1e7ddfe7c0fadad4cbcedf2ed8b84e
gargamal <pellierdaniel@free.fr>
2019-01-27 22:40:37 +0100

    [fix] - Erreur d'orthographe sur le mot accueil - Erreur sur le formualire de contact, le menu ne se rafraichissait pas. [evol] - mise en place de l'envoie de mail. - fichier js boostrap et jquery mis sur le serveur et non plus par cdn.

--- www/javascript/index.js
+++ www/javascript/index.js
@@ -1,25 +1,25 @@
 function Display() {
-    var createPhoneNumber = function (myLinkNavBar) {
-        var liPhoneNumber = document.createElement("li");
+    const createPhoneNumber = function (myLinkNavBar) {
+        let liPhoneNumber = document.createElement("li");
         liPhoneNumber.id = "myPhoneNumber";
         myLinkNavBar.appendChild(liPhoneNumber);
         return liPhoneNumber;
     };
 
-    var createContactMe = function (myLinkNavBar) {
-        var liContactMe = document.createElement("li");
+    const createContactMe = function (myLinkNavBar) {
+        let liContactMe = document.createElement("li");
         liContactMe.id = "myContactMe";
         liContactMe.innerHTML = "<a href='contact.html'><span class='fa fa-envelope' aria-hidden='true'></span><span> contactez nous</span></a>";
         myLinkNavBar.appendChild(liContactMe);
         return liContactMe;
     };
 
-    var fctNavBar = function (index) {
-        var myLinkNavBar = document.getElementById("myLinkNavBar");
-        var myContactMe = document.getElementById("myContactMe");
-        var myTwitterLink = document.getElementById("myTwitterLink");
-        var myFacebookLink = document.getElementById("myFacebookLink");
-        var phoneNumber = document.getElementById("myPhoneNumber");
+    const navBar = function (index) {
+        let myLinkNavBar = document.getElementById("myLinkNavBar");
+        let myContactMe = document.getElementById("myContactMe");
+        let myTwitterLink = document.getElementById("myTwitterLink");
+        let myFacebookLink = document.getElementById("myFacebookLink");
+        let phoneNumber = document.getElementById("myPhoneNumber");
         if (!phoneNumber) {
             phoneNumber = createPhoneNumber(myLinkNavBar);
         }
@@ -38,14 +38,14 @@ function Display() {
             phoneNumber.innerHTML = "<a href='tel:+33243237149'><span class='fa fa-phone' aria-hidden='true'></span><span> 02 43 23 71 49</span></a>";
         } else if (window.innerWidth >= 993) {
             if (!myTwitterLink) {
-                var liTwitterLink = document.createElement("li");
+                let liTwitterLink = document.createElement("li");
                 liTwitterLink.id = "myTwitterLink";
                 liTwitterLink.innerHTML = "<a href='https://twitter.com/EurlPellier' target='_blank'><span class='fa fa-twitter' aria-hidden='true'></span></a>";
                 myLinkNavBar.appendChild(liTwitterLink);
             }
 
             if (!myFacebookLink) {
-                var liFacebookLink = document.createElement("li");
+                let liFacebookLink = document.createElement("li");
                 liFacebookLink.id = "myFacebookLink";
                 liFacebookLink.innerHTML = "<a href='https://www.facebook.com/fabrice.pellier.3?fref=ts' target='_blank'><span class='fa fa-facebook-official' aria-hidden='true'></span></a>";
                 myLinkNavBar.appendChild(liFacebookLink);
@@ -54,11 +54,11 @@ function Display() {
             phoneNumber.innerHTML = "<a href='#'><span class='fa fa-phone' aria-hidden='true'></span></a>";
 
             if (index === 100) {
-                liContactMe.setAttribute("class", "active");
+                myContactMe.setAttribute("class", "active");
             } else {
-                var nivBar = document.getElementById("menuNavBar");
-                var liMenu = nivBar.getElementsByTagName("li");
-                for (var i = 0; i < liMenu.length; i++) {
+                let nivBar = document.getElementById("menuNavBar");
+                let liMenu = nivBar.getElementsByTagName("li");
+                for (let i = 0; i < liMenu.length; i++) {
                     if (i === index) {
                         liMenu[i].setAttribute("class", "active");
                         break;
@@ -69,6 +69,6 @@ function Display() {
     };
 
     return {
-        navBar: fctNavBar
+        navBar,
     }
 }
